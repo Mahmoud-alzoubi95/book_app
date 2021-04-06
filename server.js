a6165cf6baa5f59d4119e72d4badb70b0918d874
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-04-06 23:06:43 +0300

    many changes

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ client.on('error', err => console.error(err));
 // app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static( "./puplic"));
-
-app.post('/searches', createSearch);
+app.post('/searches',createSearch);
+app.post('/books',saveData);
+app.get('/book/:id',getMyBook);
+app.get('/searches/new',showForm);
 app.get('/', renderHomePage);
 app.get('/hello', renderHomePage);
-app.get('/searches/new', showForm);
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 // app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
@@ -43,8 +44,9 @@ function Book(info) {
    const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
    this.thumbnail=info.imageLinks?info.imageLinks.thumbnail:'https://i.imgur.com/J5LVHEL.jpg';
    this.title = info.title || 'No title available';
-   this.authors=info.authors;
-   this.description=info.description;
+   this.authors=info.authors ||'No authors available';
+   this.isbn = info.industryIdentifiers[0].identifiers || 'No ISBN available'; 
+   this.description=info.description || 'No description available' ;
 }
 
 function renderHomePage(request, response) {
@@ -59,8 +61,40 @@ function renderHomePage(request, response) {
   // response.render('pages/index');
 }
 
-function showForm(request, response) {
+
+function showForm(request, response){
+
   response.render('pages/searches/new.ejs');
+
+}
+function getMyBook(request, response) {
+  const id= request.params.id;
+  const myReq='SELECT * FROM books WHERE id=$1'
+  const idValue= [id];
+
+  client.query(myReq,idValue).then(results=>{
+    console.log(results.rows[0]);
+response.render('pages/books/detail', {results:results.rows[0]})
+  }).catch(error=>{
+    console.log('ERROR',error)
+  })
+
+}
+
+function saveData(request, response) {
+
+  const { title,ISPN, authors, description , image_url } = request.body;
+
+  const sqlQuery = 'INSERT INTO books (title,ISPN, authors, description , image_url ) VALUES($1,$2,$3,$4,$5) RETURNING id;';
+  const safeValues = [title,ISPN, authors, description , image_url ];
+
+  client.query(sqlQuery, safeValues).then(results => {
+    response.redirect(`/book/${results.rows[0].id}`);
+  }).catch(error=>{
+    console.log('ERROR',error)
+  })
+
+  // response.render('pages/searches/new.ejs');
 }
 
 function createSearch(request, response) {
